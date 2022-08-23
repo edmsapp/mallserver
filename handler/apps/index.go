d8a9b25fb6a120f228c30e47134759fcf2d0c7d7
Richard <wangleihd@gmail.com>
2022-08-23 20:13:43 +0800

    Add new procduct lists

---
+++ handler/apps/index.go
@@ -0,0 +1,38 @@
+package apps
+
+import (
+	. "mallServer/handler"
+	"mallServer/pkg/errno"
+	"mallServer/service"
+
+	"github.com/gin-gonic/gin"
+	"github.com/zxmrlc/log"
+)
+
+// @Summary List the app in the database
+// @Description List app product
+// @Tags user
+// @Accept  json
+// @Produce  json
+// @Param user body app.ListRequest true "List app"
+// @Success 200 {object} user.SwaggerProductListResponse "{"code":0,"message":"OK","data":{"totalCount":1,"userList":[{"id":0,"username":"admin","random":"user 'admin' get random string 'EnqntiSig'","password":"$2a$10$veGcArz47VGj7l9xN7g2iuT9TF21jLI1YGXarGzvARNdnt4inC9PG","createdAt":"2018-05-28 00:25:33","updatedAt":"2018-05-28 00:25:33"}]}}"
+// @Router /productlists [get]
+func ProductLists(c *gin.Context) {
+	log.Info("ProductLists function called.")
+	var r ProductListResponse
+	if err := c.Bind(&r); err != nil {
+		SendResponse(c, errno.ErrBind, nil)
+		return
+	}
+
+	infos, count, err := service.ListUser(r.Username, r.Offset, r.Limit)
+	if err != nil {
+		SendResponse(c, err, nil)
+		return
+	}
+
+	SendResponse(c, nil, ProductListRequest{
+		TotalCount: count,
+		UserList:   infos,
+	})
+}
